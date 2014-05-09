694b305ce9362bddf3d42df9cca89357f6241f01
tuan08 <tuan08@gmail.com>
2014-05-09 21:01:56 +0700

    add statistic util code

---
+++ code/lib/common/src/main/java/com/neverwinterdp/util/statistic/StatisticVisitor.java
@@ -0,0 +1,8 @@
+package com.neverwinterdp.util.statistic;
+
+/**
+ * $Author: Tuan Nguyen$ 
+ **/
+public interface StatisticVisitor {
+	public void onVisit(Statistics statistics, Statistic statistic) ;
+}
