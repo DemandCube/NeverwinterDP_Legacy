afe42b75c319c14a67651a1124bf5788971a9081
tuan08 <tuan08@gmail.com>
2014-05-14 10:35:57 +0700

    add hazelcast sample

---
+++ code/lib/common/src/test/java/com/neverwinterdp/server/hazelcast/MapUnitTest.java
@@ -0,0 +1,30 @@
+package com.neverwinterdp.server.hazelcast;
+
+import org.junit.Test;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.XmlConfigBuilder;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+
+public class MapUnitTest {
+  @Test
+  public void run() throws Exception {
+    HazelcastInstance instance1 = Hazelcast.newHazelcastInstance(createConfig(5700));
+    HazelcastInstance instance2 = Hazelcast.newHazelcastInstance(createConfig(5700));
+    System.out.println("Members: "+ instance1.getCluster().getMembers().size()) ;
+    
+    
+    IMap<String, Object> map1 = instance1.getMap("default");
+    map1.put("test", "test") ;
+    
+    IMap<String, Object> map2 = instance2.getMap("default");
+    System.out.println("map2 get test = " + map2.get("test"));
+  }
+  
+  Config createConfig(int port) {
+    Config cfg = new XmlConfigBuilder().build();
+    return cfg ; 
+  }
+}
---
+++ code/lib/common/src/test/java/com/neverwinterdp/server/hazelcast/TaskUnitTest.java
@@ -0,0 +1,74 @@
+package com.neverwinterdp.server.hazelcast;
+
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.XmlConfigBuilder;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.HazelcastInstanceAware;
+import com.hazelcast.core.IExecutorService;
+import com.hazelcast.core.Member;
+
+public class TaskUnitTest  {
+  HazelcastInstance instance1, instance2, instance3 ;
+  
+  @Before
+  public void setup() {
+    // Configure
+    Config config = new XmlConfigBuilder().build();
+    // Set call timeout to 5 seconds to make the problem appear quicker
+    config.setProperty("hazelcast.operation.call.timeout.millis", "5000");
+
+    // Create two Hazelcast instances
+    instance1 = Hazelcast.newHazelcastInstance(config);
+    instance2 = Hazelcast.newHazelcastInstance(config);
+    instance3 = Hazelcast.newHazelcastInstance(config);
+  }
+  
+  @After
+  public void teardown() {
+    instance1.getLifecycleService().shutdown();
+    instance2.getLifecycleService().shutdown();
+    instance3.getLifecycleService().shutdown();
+  }
+  
+  @Test
+  public void testGetOnLongRunningTask() throws Exception {
+    // Submit the hello task on instance 1
+    IExecutorService exService = instance1.getExecutorService("default");
+    Map<Member, Future<Object>> futures = exService.submitToAllMembers(new HelloTask());
+    Iterator<Future<Object>> itr = futures.values().iterator() ;
+    while(itr.hasNext()) {
+      Object result = itr.next().get(25, TimeUnit.SECONDS);
+      System.out.println(result);
+    }
+  }
+
+  private static class HelloTask implements Callable<Object>, Serializable, HazelcastInstanceAware {
+    private static final long serialVersionUID = 1L;
+
+    transient private HazelcastInstance hazelcastInstance ;
+    
+    public Object call() throws Exception {
+      Member member = hazelcastInstance.getCluster().getLocalMember() ;
+      System.out.println(member + " task: Started. Sleep for 1 seconds");
+      Thread.sleep(1000);
+      System.out.println(member + "task: Finished");
+      return "Hello from " + member;
+    }
+
+    public void setHazelcastInstance(HazelcastInstance hazelcastInstance) {
+      this.hazelcastInstance = hazelcastInstance ;
+    }
+  }
+}
