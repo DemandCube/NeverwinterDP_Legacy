37803eac5acb9e6c5551ec67b32f16663be46eb6
Tuan Nguyen <tuan08@gmail.com>
2014-09-01 09:40:29 +0700

    add yourkit configuration profiler options

--- src/app/bin/service.sh
+++ src/app/bin/service.sh
@@ -34,6 +34,9 @@ if $cygwin; then
   APP_DIR=`cygpath --absolute --windows "$APP_DIR"`
 fi
 
+#Yourkit Profiler
+YOURKIT_LIB="/Users/Tuan/Tools/YourKit-2013.app/bin/mac/libyjpagent.jnilib"
+#YOURKIT_OPTS="-agentpath:$YOURKIT_LIB=disablestacktelemetry,disableexceptiontelemetry,builtinprobes=none,delay=10000,sessionname=NeverwinterDP"
 
 JAVA_OPTS="-Xshare:auto -Xms128m -Xmx256m -XX:-UseSplitVerifier"
 
@@ -45,7 +48,7 @@ LOG_OPT="-Dlog4j.configuration=file:$APP_DIR/config/log4j.properties"
 MAIN_CLASS="com.neverwinterdp.server.Server"
 
 function startServer {
-  nohup $JAVACMD -Djava.ext.dirs=$APP_DIR/libs $APP_OPT $LOG_OPT $MAIN_CLASS "$@" <&- &>/dev/null &
+  nohup $JAVACMD -Djava.ext.dirs=$APP_DIR/libs $APP_OPT $YOURKIT_OPTS $LOG_OPT $MAIN_CLASS "$@" <&- &>/dev/null &
   #printf '%d' $! > $SERVER_NAME.pid
 }
 
